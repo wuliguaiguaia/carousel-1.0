15d00d1b5fb9c8db99d296d410e2184561dfa17c
wuliguaiguaia <1944063509@qq.com>
2018-11-20 18:50:38 +0800

    optimization

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-// todo:
+let position = 0, $img = $(".container img"), len = $img.length,timer = null;
+$img.first().addClass("current").siblings().addClass("enter");
 
-let position, timer, $img, len, $dot;
-Init();
-
-
-$dot.each((x, item) => {
-    $(item).on("click", function () {
-        position = x;
-        move(position);
-    })
-})
-
-
-$('#wrapper').on("mouseleave", function () {
-    timer = setInterval(function () {
-        move(position);
-        position++;
-    }, 3000)
-}).on("mouseenter", function () {
-    clearInterval(timer);
-}).trigger("mouseleave")
+timer = setInterval(() => {
+    setLeave($img.eq(formateRange(position))).one("transitionend",(e)=>{
+        setEnter($(e.currentTarget));
+    });
+    setCurrent($img.eq(formateRange(position+1)));
+    position++;
+}, 3000);
 
 
 
@@ -32,36 +20,40 @@ $('#wrapper').on("mouseleave", function () {
 
 
 
-function move(i) {
-    setEnter(setRange(i + 1));
-    setCurrent(setRange(i))
-    setLeave(setRange(i - 1));
-}
 
 
-function setRange(x) {
-    if (x >= len) {
-        x = x % len;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function formateRange(i){
+    if(i >= len){
+        i = i % len;
     }
-    return x;
+    return i;
 }
 
-function Init() {
-    position = 0;
-    timer = null;
-    $img = $(".container img");
-    len = $img.length;
-    $dot = $(".dot span");
+function setCurrent($node){
+    return $node.removeClass("enter").addClass("current");
 }
-
-function setLeave(i) {
-    return $img.eq(i).removeClass("enter current").addClass("leave");
+function setLeave($node){
+    return $node.removeClass("current").addClass("leave")
 }
-
-function setCurrent(i) {
-    return $img.eq(i).removeClass("leave enter").addClass("current")
-}
-
-function setEnter(i) {
-    return $img.eq(i).removeClass("current leave").addClass("enter")
+function setEnter($node){
+    return $node.removeClass("leave").addClass("enter");
 }
