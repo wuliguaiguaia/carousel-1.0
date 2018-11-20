7c59149ec5c8ed9aabd28aae85d10555cfb8b735
wuliguaiguaia <1944063509@qq.com>
2018-11-20 19:39:27 +0800

    :bug:leave page

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-let position = 0, $img = $(".container img"), len = $img.length,timer = null;
+let position = 0, $img = $(".container img"), len = $img.length, timer = null;
 $img.first().addClass("current").siblings().addClass("enter");
-
 timer = setInterval(() => {
-    setLeave($img.eq(formateRange(position))).one("transitionend",(e)=>{
-        setEnter($(e.currentTarget));
-    });
-    setCurrent($img.eq(formateRange(position+1)));
-    position++;
+    interval()
 }, 3000);
 
+$(document).on("visibilitychange", function () {
+    if (document.hidden) {
+        clearInterval(timer);
+    } else {
+        timer = setInterval(() => {
+            interval()
+        }, 3000)
+    }
+})
+
+function interval() {
+    setLeave($img.eq(formateRange(position))).one("transitionend", (e) => {
+        setEnter($(e.currentTarget));
+    });
+    setCurrent($img.eq(formateRange(position + 1)));
+    position++;
+}
+
+
+// test
+setInterval(() => {
+    console.log(new Date());
+}, 500);
 
 
 
@@ -40,20 +58,19 @@ timer = setInterval(() => {
 
 
 
-
-function formateRange(i){
-    if(i >= len){
+function formateRange(i) {
+    if (i >= len) {
         i = i % len;
     }
     return i;
 }
 
-function setCurrent($node){
+function setCurrent($node) {
     return $node.removeClass("enter").addClass("current");
 }
-function setLeave($node){
+function setLeave($node) {
     return $node.removeClass("current").addClass("leave")
 }
-function setEnter($node){
+function setEnter($node) {
     return $node.removeClass("leave").addClass("enter");
 }
